ade5c05a21dabb3d5714a6fcd665dba99abf1597
nurfiiitriiiyah <nurfiitriiyah@gmail.com>
2019-05-22 13:42:57 +0700

    css nya beloman backend nya golang

--- src/components/components_login/login.vue
+++ src/components/components_login/login.vue
@@ -5,7 +5,8 @@
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand">
-              <img src="../../../src/assets/img/stisla-fill.svg" alt="logo" width="100" class="shadow-light rounded-circle">
+              <img src="../../../src/assets/img/stisla-fill.svg" alt="logo" width="100"
+                   class="shadow-light rounded-circle">
             </div>
             <div class="card card-primary">
               <div class="card-header"><h4>Login</h4></div>
@@ -14,7 +15,8 @@
                 <form method="POST" action="#" class="needs-validation" novalidate="" onsubmit="return false;">
                   <div class="form-group">
                     <label for="email">Email</label>
-                    <input v-model="todo.username" id="email" type="email" class="form-control" name="email" tabindex="1" required autofocus>
+                    <input v-model="todo.Username" id="email" type="email" class="form-control" name="email"
+                           tabindex="1" required autofocus>
                     <div class="invalid-feedback">
                       Please fill in your email
                     </div>
@@ -24,9 +26,10 @@
                     <div class="d-block">
                       <label for="password" class="control-label">Password</label>
                       <div class="float-right">
-                                            </div>
+                      </div>
                     </div>
-                    <input v-model="todo.passwd" id="password" type="password" class="form-control" name="password" tabindex="2" required>
+                    <input v-model="todo.Password" id="password" type="password" class="form-control" name="password"
+                           tabindex="2" required>
                     <div class="invalid-feedback">
                       please fill in your password
                     </div>
@@ -59,7 +62,7 @@
 
 <script>
   const axios = require('axios');
-  const API_URL = 'http://localhost:10002/login';
+  const API_URL = 'http://localhost:10005/login';
 
   export default {
     name: 'login',
@@ -72,16 +75,16 @@
     transitionName: 'fade',
     methods: {
       async login() {
-        try{
-          var submitLogin = await axios.post(API_URL,this.todo)
-          if(submitLogin.data.status === "nok"){
-            this.$swal("Gagal","Silahkan Cek Username & Password","error");
-          }else{
-            document.cookie = "token="+"fitri";
-            this.$router.push('/dash');
-          }
-        }catch (e) {
-          console.log(e)
+        try {
+          axios.post(API_URL, this.todo).then((response) => {
+              document.cookie = "token="+response.data.token;
+              this.$router.push('/dash');
+            },
+            (error) => {
+              this.$swal("Failed !","Please Check Username and Password","error");
+            })
+        } catch (e) {
+          this.$swal("Failed",e,"error");
         }
       },
     },
